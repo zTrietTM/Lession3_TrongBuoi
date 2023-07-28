1c7f7aa8ecf725c5a39e9250b221b105109f44b8
TRIET <TRIET@192.168.100.204>
2023-07-28 16:16:41 +0700

    update lan 2

---
+++ Test Suites/PushToGit/UpdateToGitHub.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UpdateToGitHub</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8d01013e-4173-4c32-aee7-fb1bc8e61fc3</testSuiteGuid>
+   <testCaseLink>
+      <guid>e6bbaa8e-90da-4cd9-a606-bd98561602f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_RegisterAccount</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f7f74075-6817-415b-93f6-1ca7c9892a5f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2e90a4b2-844e-4749-9cf5-e11f463a595b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c083f43c-9658-4d0e-89b9-eac73d38f94a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6c0542db-71ec-4f09-b633-454fa12d73ac</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8b1c3019-726a-496a-8f85-f7b8ce4cd96a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
